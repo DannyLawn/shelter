bf3b929f435805f8d27f62bb38e05ab0bd1af63d
Daniil Nikitin <danny.lawn@yandex.ru>
2022-10-24 20:01:28 +0300

    Разметить основные разделы сайта

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,15 +4,18 @@ import Footer from '../Footer/Footer';
 import Intro from '../Intro/Intro';
 import DangerZone from '../DangerZone/DangerZone';
 import News from '../News/News';
+import Shelter from '../Shelter/Shelter';
 import styles from './App.module.scss';
 
 function App() {
+
   return (
     <div className={styles.page}>
-      <Header />
+      <Header/>
       <main className={styles.content}>
         <Intro />
         <News />
+        <Shelter /> 
         <DangerZone />
       </main>
       <Footer />

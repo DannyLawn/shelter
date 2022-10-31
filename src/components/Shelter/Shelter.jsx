a9f1b981169ebb87d331880de490391a29b87395
Daniil Nikitin <danny.lawn@yandex.ru>
2022-10-31 04:40:07 +0300

    Внесены правки

--- src/components/Shelter/Shelter.jsx
+++ src/components/Shelter/Shelter.jsx
@@ -8,7 +8,7 @@ export default function Shelter() {
     <section className={styles.shelter}>
       <h2 className={styles.shelter__title}>Укрытие</h2>
       <img className={styles.shelter__image} src={shelterImg} alt="Бункер." />
-      <p className={styles.shelter__text}>Сохраняйте спокойствие, все будет в полном ажуре. В то же время, при экстренной ситуации, лучше полагаться не только на случай. Списки укрытий будут пополняться модерацией по мере необходимости. Данные проверяется вручную. Статусы свободных мест обновляются.</p> 
+      <p className={styles.shelter__text}>Все будет в полном ажуре. В то же время, при экстренной ситуации, лучше полагаться не только на случай. Списки укрытий будут пополняться модерацией по мере необходимости. Данные проверяется вручную. Статусы свободных мест обновляются.</p> 
         <p className={styles.shelter__capture}>Выберите город</p>
         <select className={styles.shelter__select} name="choice">
           <option className={styles.shelter__selectOption} value="first">Херсон</option>

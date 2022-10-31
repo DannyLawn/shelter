f7bcedd5d5fd9653c2f67486173dce8d958880fb
Daniil Nikitin <danny.lawn@yandex.ru>
2022-10-31 04:19:36 +0300

    Добавлен переход по разделам и многое другое

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,16 +9,56 @@ import styles from './App.module.scss';
 
 function App() {
 
+  const [newsState, setNewsState] = React.useState(false);
+  const [shelterState, setShelterState] = React.useState(false);
+  const [dZoneState, setDZoneState] = React.useState(false);
+  const [mainIntroState, setMainIntroState ] = React.useState(true);
+  const [rulesButtonState, setRulesButtonState] = React.useState(true);
+ 
+
+  function handleNewsPage() {
+    setNewsState(true);
+    setShelterState(false);
+    setDZoneState(false);
+    setMainIntroState(false);
+    setRulesButtonState(false);
+  }
+
+  function handleShelterPage() {
+    setShelterState(true);
+    setNewsState(false);
+    setDZoneState(false);
+    setMainIntroState(false);
+    setRulesButtonState(false);
+  }
+
+  function handleDZonePage() {
+    setDZoneState(true);
+    setShelterState(false);
+    setNewsState(false);
+    setMainIntroState(false);
+    setRulesButtonState(false);
+  }
+
+  function openMainIntro() {
+    setMainIntroState(true);
+    setRulesButtonState(true);
+    setDZoneState(false);
+    setShelterState(false);
+    setNewsState(false);
+  }
+
+
   return (
     <div className={styles.page}>
-      <Header/>
+      <Header handleNewsPage={handleNewsPage} handleShelterPage={handleShelterPage} handleDZonePage={handleDZonePage} openMainIntro={openMainIntro} />
       <main className={styles.content}>
-        <Intro />
-        <News />
-        <Shelter /> 
-        <DangerZone />
+        {Boolean(mainIntroState) && (<Intro />)}
+        {Boolean(newsState) && (<News />)}
+        {Boolean(shelterState) && (<Shelter />)}
+        {Boolean(dZoneState) && <DangerZone />}
       </main>
-      <Footer />
+      <Footer rulesButtonState={rulesButtonState} />
     </div>
   );
 }

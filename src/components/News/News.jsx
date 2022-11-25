e80c837194516f62d4460f90e9d9597858e11667
Daniil Nikitin <danny.lawn@yandex.ru>
2022-11-26 02:32:33 +0300

    Улучшена секция news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -6,25 +6,16 @@ export default function News() {
 
   const newsCount = newsList.length - 1;
   const [news, setNews] = React.useState({active: 0});
-  const [activeNextBtn, setActiveNextBtn] = React.useState(true);
-  const [activePrevBtn, setActiveprevBtn] = React.useState(false);
-
+  
   const handleNextNews = () => {
     if (news.active < newsCount) {
       setNews({active: news.active + 1})
-      setActiveprevBtn(true);
-    } else {
-      setActiveNextBtn(false);
-    }
-  }
+    } 
+  } 
 
   const handlePrevNews = () => {
     if (news.active > 0) {
       setNews({active: news.active - 1})
-      setActiveNextBtn(true);
-    } else {
-      setNews({active: 0})
-      setActiveprevBtn(false);
     }
   }
 
@@ -33,12 +24,12 @@ export default function News() {
       <h2 className={styles.news__sectionTitle}>Новости</h2>
       <p className={styles.news__captureCount}>{`${news.active + 1} / ${newsCount + 1}`}</p>
       <ul className={styles.news__container}>
-      {activePrevBtn?(<button className={`${styles.news__button} ${styles.news__button_hover}`} onClick={handlePrevNews}/>):(<button className={`${styles.news__button} ${styles.news__button_disable}`} disabled/>)}
+      {news.active!==0?(<button className={`${styles.news__button} ${styles.news__button_hover}`} onClick={handlePrevNews}/>):(<button className={`${styles.news__button} ${styles.news__button_disable}`} disabled/>)}
         <li className={styles.news__item}>
           <h2 className={styles.news__title}>{newsList[news.active].title}</h2>
           <p className={styles.news__text}>{newsList[news.active].text}</p>
         </li>
-        {activeNextBtn?(<button className={`${styles.news__button} ${styles.news__button_next} ${styles.news__button_hover}`} onClick={handleNextNews}/>):(<button className={`${styles.news__button} ${styles.news__button_next} ${styles.news__button_disable}`} disabled/>)}
+        {news.active!==newsCount?(<button className={`${styles.news__button} ${styles.news__button_next} ${styles.news__button_hover}`} onClick={handleNextNews}/>):(<button className={`${styles.news__button} ${styles.news__button_next} ${styles.news__button_disable}`} disabled/>)}
       </ul>
     </section>
   );
